25a0561a316833cd88ec14c1c80116e06e0ed24b
logeshwaran0404 <logeshwaran7129@gmail.com>
2025-04-02 11:07:37 +0530

    initial commit

---
+++ src/main/java/com/mobicomm/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.mobicomm.controller;
+
+import com.mobicomm.model.Message;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", new Message("Welcome to Spring MVC with Thymeleaf!"));
+        return "home";
+    }
+    
+    @PostMapping("/submit")
+    public String submitMessage(@ModelAttribute Message message, Model model) {
+        model.addAttribute("message", message);
+        return "home";
+    }
+}
